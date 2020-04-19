be82c3c08fa43bb26c7b5795acf9ff21d4a554cc
simpson <hisimpson90@gmail.com>
2020-04-19 23:36:41 +0900

    Add function for uploading video.

---
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { Typography, Button, Form, message, Input, Icon } from "antd";
+import Dropzone from "react-dropzone";
+import axios from "axios";
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
+const privateOptions = [
+  { value: 0, label: "Private" },
+  { value: 1, label: "Public" },
+];
+
+const categoryOptions = [
+  { value: 0, label: "Film & Animation" },
+  { value: 1, label: "Auto & Vehicles" },
+  { value: 2, label: "Music" },
+  { value: 3, label: "Pets & Animals" },
+];
+
+function VideoUploadPage() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [Private, setPrivate] = useState(0);
+  const [Category, setCategory] = useState(0);
+
+  const onTitleChange = (e) => {
+    setTitle(e.currentTarget.value);
+  };
+
+  const onDescriptionChange = (e) => {
+    setDescription(e.currentTarget.value);
+  };
+
+  const onPrivateChange = (e) => {
+    setPrivate(e.currentTarget.value);
+  };
+
+  const onCategoryChange = (e) => {
+    setCategory(e.currentTarget.value);
+  };
+
+  const onDrop = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: { "content-type": "multipart/form-data" },
+    };
+    formData.append("file", files[0]);
+    axios.post("/api/videos/uploadfiles", formData, config).then((res) => {
+      if (res.data.success) {
+        console.log(res.data);
+      } else {
+        alert("Failed to Upload video.");
+      }
+    });
+  };
+
+  return (
+    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+        <Title level={2}>Upload Video</Title>
+      </div>
+      <Form onSubmit>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          {/* Drop zone */}
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={100000000}>
+            {({ getRootProps, getInputProps }) => (
+              <div
+                style={{
+                  width: "300px",
+                  height: "240px",
+                  border: "1px solid lightgray",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+                {...getRootProps()}
+              >
+                <input {...getInputProps()} />
+                <Icon type="plus" style={{ fontSize: "3rem" }} />
+              </div>
+            )}
+          </Dropzone>
+          {/* Thumbnail */}
+          <div>
+            <img src alt />
+          </div>
+        </div>
+        <label>Title</label>
+        <Input onChange={onTitleChange} value={title} />
+        <label>Description</label>
+        <TextArea onChange={onDescriptionChange} value={description} />
+        <select onChange={onPrivateChange}>
+          {privateOptions.map((item, idx) => (
+            <option key={idx} value={item.value}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+        <select onChange={onCategoryChange}>
+          {categoryOptions.map((item, idx) => (
+            <option key={idx} value={item.value}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+        <Button type="primary" size="large" onClick>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default VideoUploadPage;
